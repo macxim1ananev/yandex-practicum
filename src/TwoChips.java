cac7eb5c0460f8be9cd1e566df38785b43e9e75c
maxim <maksim.ananev.1994@mail.ru>
2023-05-17 10:20:58 +0500

    Две фишки второй способ

--- src/TwoChips.java
+++ src/TwoChips.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 //Две фишки
 public class TwoChips {
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int[] chips = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -14,12 +14,39 @@ public class TwoChips {
 
     }
 
-    private static String findNumbers(int n, int[] chips, int number){
-        outt :for (int i = 0; i < chips.length; i++)  {
+    private static String findNumbers(int n, int[] chips, int number) {
+        outt:
+        for (int i = 0; i < chips.length; i++) {
             for (int j = i + 1; j < chips.length; j++) {
-                if (chips[i] + chips[j]==number){
+                if (chips[i] + chips[j] == number) {
                     return chips[i] + " " + chips[j];
                 }
+            }
+        }
+        return "None";
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] chips = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int number = Integer.parseInt(scanner.nextLine());
+        Arrays.sort(chips);
+        System.out.println(findNumbers2(n,chips,number));
+    }
+
+    private static String findNumbers2(int n, int[] chips, int number){
+        int left = 0;
+        int right = chips.length-1;
+        while (left<right){
+            int currentSum = chips[left] + chips[right];
+            if (currentSum==number){
+                return chips[left] + " " + chips[right];
+            } else if (currentSum < number) {
+                left+=1;
+            } else {
+                right -=1;
             }
         }
         return "None";
