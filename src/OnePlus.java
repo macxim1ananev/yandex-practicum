b8d26e781639b54d27de561c04dd4e9ff5fc9072
maxim <maksim.ananev.1994@mail.ru>
2023-06-07 15:39:42 +0500

    one plus

---
+++ src/OnePlus.java
@@ -0,0 +1,23 @@
+public class OnePlus {
+    public static void main(String[] args) {
+        int[] arr = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,9};
+        int[] res = plusOne(arr);
+        for (int i = 0; i <res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
